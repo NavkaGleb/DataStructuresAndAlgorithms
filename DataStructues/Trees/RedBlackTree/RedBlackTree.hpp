5b992250b7f008a72c8e71fa60f139992a8c6baf
NavkaGleb <navka.gleb@gmail.com>
2021-02-01 12:13:13 +0200

    Add accessors to RedBlackTree::Node. Add methods prototipes of RedBlackTree

--- DataStructues/Trees/RedBlackTree/RedBlackTree.hpp
+++ DataStructues/Trees/RedBlackTree/RedBlackTree.hpp
@@ -20,6 +20,14 @@ namespace Ng {
                           Node* right = nullptr);
             virtual ~Node();
 
+            [[nodiscard]] inline const T& GetValue() const { return m_Value; }
+            [[nodiscard]] inline const Color& GetColor() const { return m_Color; }
+            [[nodiscard]] inline const Node* GetParent() const { return m_Parent; }
+            [[nodiscard]] inline const Node* GetLeft() const { return m_Left; }
+            [[nodiscard]] inline const Node* GetRight() const { return m_Right; }
+
+            friend class RedBlackTree;
+
         private:
             T     m_Value;
             Color m_Color;
@@ -29,19 +37,25 @@ namespace Ng {
 
         }; // class Node
 
+        explicit RedBlackTree(const T& value);
         explicit RedBlackTree(Node* root = nullptr);
         virtual ~RedBlackTree();
 
         [[nodiscard]] inline Node* GetRoot() { return m_Root; }
         [[nodiscard]] inline const Node* GetRoot() const { return m_Root; }
+        [[nodiscard]] inline int GetNodes() const { return m_Nodes; }
+
+        [[nodiscard]] bool IsExists(const T& value) const;
 
         Node* PushNode(const T& value);
         void PopNode(const T& value);
 
     private:
-        
+        void RotateLeft(Node* node);
+        void RotateRight(Node* node);
 
         Node* m_Root;
+        int   m_Nodes;
 
     }; // class RedBlackTree
 
@@ -76,8 +90,14 @@ namespace Ng {
     /// class RedBlackTree
     //////////////////////////////////////////////////////////////////////////////
     template <typename T>
+    RedBlackTree<T>::RedBlackTree(const T& value) :
+        m_Root(new Node(value)),
+        m_Nodes(1) {}
+
+    template <typename T>
     RedBlackTree<T>::RedBlackTree(Node* root) :
-        m_Root(root) { }
+        m_Root(root),
+        m_Nodes(root ? 1 : 0) { }
 
     template <typename T>
     RedBlackTree<T>::~RedBlackTree() {
@@ -85,7 +105,39 @@ namespace Ng {
     }
 
     template <typename T>
-    Node* RedBlackTree<T>::PushNode(const T& value) {
+    bool RedBlackTree<T>::IsExists(const T& value) const {
+        Node* node = m_Root;
+
+        while (node && value != node->Value) {
+            if (value < node->Value)
+                node = node->Left;
+            else
+                node = node->Right;
+        }
+
+        return node;
+    }
+
+    template <typename T>
+    typename RedBlackTree<T>::Node* RedBlackTree<T>::PushNode(const T& value) {
+        Node* node = new Node(value);
+
+        if (!m_Root)
+            return m_Root = node;
+    }
+
+    template <typename T>
+    void RedBlackTree<T>::PopNode(const T& value) {
+
+    }
+
+    template <typename T>
+    void RedBlackTree<T>::RotateLeft(Node* node) {
+
+    }
+
+    template <typename T>
+    void RedBlackTree<T>::RotateRight(Node* node) {
 
     }
 
